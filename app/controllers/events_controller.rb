0d64c46dfc4cc72a9db7e724455a2130c68fd6d7
VenkatramanC <venkatraman.c9@gmail.com>
2015-11-15 14:21:01 +0530

    Events added to a page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,6 +3,20 @@ class EventsController < ApplicationController
     @event = Event.pretty_new
   end
 
+
+  def create
+    @event = Event.new(event_params) 
+    @event.start_time = parse_time(:event , "start_time") 
+    @event.end_time =  parse_time(:event , "end_time")
+    @event.save
+    redirect to events_path 
+  end
+
   def index
   end
-end
+
+  private
+     def event_params
+        params.require(:event).permit(:title , :description)
+     end
+  end
